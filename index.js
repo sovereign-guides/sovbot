4ace603bfeaea079a10302667e35460cbf93582e
Zay Kenyon <isaac@kenyon.co.uk>
2022-07-24 18:33:10 -0400

    feat(*): initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Require the necessary discord.js classes
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const fs = require('node:fs');
+
+// Create a new client instance
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds
+	]
+});
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+// Login to Discord with your client's token
+client.login(token);
