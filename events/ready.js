73995132aae531a7e0e9a6579834c730605dae8a
Zay Kenyon <isaac@kenyon.co.uk>
2022-07-24 21:26:02 -0400

    feat(status): display vct command

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,10 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		client.user.setActivity('VCT with /vct', { type: ActivityType.Watching });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
