a7817b69c3fee220eb28bc8ecb375f3fdd362214
Zay Kenyon <isaac@kenyon.co.uk>
2022-07-25 01:21:20 -0400

    test(threads): checks message length for titling

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,9 +5,22 @@ module.exports = {
 
 		if (message.channelId !== threadChannel.id) return;
 
+		async function lengthCheck() {
+			if (!message.content.length > 100) return;
+
+			// compensates for the ` - {message.author.username}` ending
+			const threadSignatureLength = 3 + message.author.username.length;
+
+			// purposefully negative so that we index from the end of the string
+			const pointOfSlice = 100 - message.content.length - threadSignatureLength;
+
+			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+			return `${message.content.slice(0, pointOfSlice)} - ${message.author.username}`;
+		}
+
 		const thread = await threadChannel.threads.create({
 			startMessage: message,
-			name: `${message} - ${message.author.username}`,
+			name: await lengthCheck(message) || `${message} - ${message.author.username}`,
 			autoArchiveDuration: 60 * 24 * 7,
 			rateLimitPerUser: 0,
 			reason: `${message.author.tag} submitted feedback.`,
