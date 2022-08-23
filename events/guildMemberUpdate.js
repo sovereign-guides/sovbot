b4f099ba63521ffc3317b79e057122b707cc27f2
zay <isaac@kenyon.co.uk>
2022-08-23 01:39:51 +0100

    feat(welcomer): welcome message upon role application

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -3,6 +3,13 @@ module.exports = {
 	async execute(oldMember, newMember) {
 		if (oldMember.pending === true && newMember.pending !== true) {
 			newMember.roles.add('985979444869070898');
+
+			// 💬│general-chat : 797229760978747414
+			const generalChat = newMember.guild.channels.cache.get('797229760978747414');
+			generalChat.send({
+				content: `Welcome to the server ${newMember}! Want to break the ice? Tell us what your favorite skin line is!`,
+				allowedMentions: { users: [newMember.user.id] },
+			});
 		}
 	},
 };
