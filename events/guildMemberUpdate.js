192916677b18f3207f8f55bb7825ff2f1d71de6a
zay <isaac@kenyon.co.uk>
2022-08-23 02:07:58 +0100

    feat(welcomer): randomised welcome messages

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,12 +2,26 @@ module.exports = {
 	name: 'guildMemberUpdate',
 	async execute(oldMember, newMember) {
 		if (oldMember.pending === true && newMember.pending !== true) {
-			newMember.roles.add('985979444869070898');
 
-			// 💬│general-chat : 797229760978747414
+			// @Member: 985979444869070898
+			await newMember.roles.add('985979444869070898');
+
+			// #💬│general-chat: 797229760978747414
 			const generalChat = newMember.guild.channels.cache.get('797229760978747414');
-			generalChat.send({
-				content: `Welcome to the server ${newMember}! Want to break the ice? Tell us what your favorite skin line is!`,
+
+			const welcomeMessagesArray = [
+				'Tell us what your favorite skin line is!',
+				'Tell us what team you support!',
+				'Tell us who your favorite pro-player is!',
+				'Tell us what video you came from!',
+				'Tell us what your favorite gun is!',
+				'Tell us which you prefer from Phantom or Vandal!',
+			];
+
+			const welcomeMessage = welcomeMessagesArray[Math.floor(Math.random() * welcomeMessagesArray.length)];
+
+			await generalChat.send({
+				content: `Welcome to the server ${newMember}! ${welcomeMessage}`,
 				allowedMentions: { users: [newMember.user.id] },
 			});
 		}
