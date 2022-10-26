9933cfe459d001403e23870867bfc794b2440cdc
zay <isaac@kenyon.co.uk>
2022-10-26 22:27:24 +0100

    feat: Message Context

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
 	],
 });
 
