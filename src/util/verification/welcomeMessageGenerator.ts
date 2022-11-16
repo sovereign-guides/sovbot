559c765351bd403931bc1a8088e79581869ab3b6
zay <isaac@kenyon.co.uk>
2022-11-16 18:01:50 +0000

    chore(verification): remove bad questions

--- src/util/verification/welcomeMessageGenerator.ts
+++ src/util/verification/welcomeMessageGenerator.ts
@@ -1,9 +1,7 @@
 const welcomeMessagesArray: string[] = [
 	'Tell us what your favorite skin line is!',
 	'Tell us what team you support!',
-	'Tell us who your favorite pro-player is!',
 	'Tell us what video you came from!',
-	'Tell us what your favorite gun is!',
 	'Tell us which you prefer from Phantom or Vandal!'];
 
 export default function welcomeMessageGenerator(): string {
