a29568ef2434ac702110961a91731efd0d48203b
zay <isaac@kenyon.co.uk>
2022-12-11 17:23:10 +0000

    extract start-up functions

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,20 +1,29 @@
 import type { Client } from 'discord.js';
 import { Events, ActivityType } from 'discord.js';
 
+function validateClient(client: Client) {
+	if (!client.user) {
+		console.log('Client could not start.');
+		return;
+	}
+
+	console.log(`${client.user.tag} (${client.user.id}) is online!`);
+}
+
+function memberSizeActivity(client: Client) {
+	const sovServer = client.guilds.cache.get('797229760484343838');
+	if (!sovServer) {
+		return;
+	}
+
+	client.user?.setActivity(`with ${sovServer.memberCount} gamers!`, { type: ActivityType.Playing });
+}
+
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client: Client) {
-		if (!client.user) {
-			return;
-		}
-		console.log(`${client.user.tag} (${client.user.id}) is online!`);
-
-		const sovServer = await client.guilds.cache.get('797229760484343838');
-		if (!sovServer) {
-			return;
-		}
-		await client.user.setActivity(`with ${sovServer.memberCount} gamers!`, { type: ActivityType.Playing });
-
+		validateClient(client);
+		memberSizeActivity(client);
 	},
 };
