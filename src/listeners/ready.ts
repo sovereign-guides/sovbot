965cb056eeda311ddcb741c37b7e2c0a8f12fba7
zay <isaac@kenyon.co.uk>
2022-12-11 00:20:27 +0000

    feat: custom status

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,12 +1,20 @@
 import type { Client } from 'discord.js';
-import { Events } from 'discord.js';
+import { Events, ActivityType } from 'discord.js';
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client: Client) {
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		const { tag, id } = client.user!;
-		console.log(`${tag} (${id}) is online!`);
+		if (!client.user) {
+			return;
+		}
+		console.log(`${client.user.tag} (${client.user.id}) is online!`);
+
+		const sovServer = await client.guilds.cache.get('797229760484343838');
+		if (!sovServer) {
+			return;
+		}
+		await client.user.setActivity(`with ${sovServer.memberCount} gamers!`, { type: ActivityType.Playing });
+
 	},
 };
