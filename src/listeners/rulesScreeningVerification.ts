fe295bd2dc824e54abed765fec3a48d63df6e225
zay <isaac@kenyon.co.uk>
2022-12-12 17:56:17 +0000

    tidy up verification.ts

--- src/listeners/rulesScreeningVerification.ts
+++ src/listeners/rulesScreeningVerification.ts
@@ -4,8 +4,8 @@ import { Events } from 'discord.js';
 const welcomeMessagesArray: string[] = [
     'Tell us what your favorite skin line is!',
     'Tell us what team you support!',
-    'Tell us what video you came from!',
-    'Tell us which you prefer from Phantom or Vandal!'];
+    'Tell us which you prefer from Phantom or Vandal!',
+];
 
 function welcomeMessageGenerator(): string {
     return welcomeMessagesArray[Math.floor(Math.random() * welcomeMessagesArray.length)];
@@ -15,15 +15,16 @@ module.exports = {
     name: Events.GuildMemberUpdate,
     async execute(oldMember: GuildMember, newMember: GuildMember) {
         if (oldMember.pending && !newMember.pending) {
-            // @Member: 985979444869070898
+
             await newMember.roles.add('985979444869070898');
+            if (!newMember.guild.available) {
+                return;
+            }
 
-            if (!newMember.guild.available) return;
-
-            // #💬│general-chat: 797229760978747414
             const channel = await newMember.guild.channels.cache.get('797229760978747414');
-
-            if (!channel || !channel.isTextBased()) return;
+            if (!channel || !channel.isTextBased()) {
+                return;
+            }
 
             await channel.send({
                 content: `Welcome to the server ${newMember}! Want to break the ice? ${welcomeMessageGenerator()}`,
