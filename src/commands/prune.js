1401b851798ce661133472fae94aaf49f04a6d09
zay <isaac@kenyon.co.uk>
2023-06-10 22:03:04 +0100

    (/prune): replace embed with markdown

--- src/commands/prune.js
+++ src/commands/prune.js
@@ -54,20 +54,17 @@ async function sendCommandResponse(interaction, unverifiedMembersEmbed) {
 }
 
 async function pruneTheBitches(unverifiedMemberMap) {
-	const dmEmbed = new EmbedBuilder()
-		.setColor(0x0eae96)
-		.setTitle('Sovereign Guides Server Notice')
-		.setDescription('Your account has been removed from the server as you had not verified after 3 weeks of joining.' + '\n' + '\n' +
-			'You may reattempt verification at any time by rejoining the server using this link: https://discord.gg/Jb3Kdqwh8Q.' + '\n' + '\n' +
-			'If you need more assistance, please see this GIF for more instruction: https://i.imgur.com/TAEgBtg.mp4 or reach out to a member of our staff team by DMing @Sov ModMail (\'Sov ModMail\') at the top of the server\'s member list.',
-		);
+	const dmMessage = '# ⚠️ Sovereign Guides Server Notice\n' +
+		'Your account has been removed from the server as you had not verified after 3 weeks of joining.\n' +
+		'\n' +
+		'You may reattempt verification at any time by rejoining the server using this link: https://discord.gg/Jb3Kdqwh8Q\n' +
+		'\n' +
+		'If you need more assistance, please see this GIF for more instruction: <https://i.imgur.com/TAEgBtg.mp4> or reach out to a member of our staff team by DMing @Sov ModMail (\'Sov ModMail\') at the top of the server\'s member list.';
 
 	let failureCount = 0;
 
 	for (const unverifiedMember of unverifiedMemberMap.values()) {
-		await unverifiedMember.send({
-			embeds: [dmEmbed],
-		})
+		await unverifiedMember.send(dmMessage)
 			.catch(() => failureCount++)
 			.then(async () => {
 				await unverifiedMember.kick()
