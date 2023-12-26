edd1174b56240c93a7a51e0787647b02e00bc96b
zay <zaykenyon@protonmail.com>
2023-12-25 19:09:56 -0500

    feat: volunteer stage integration

--- src/modules/volunteer/events/buttonVolunteerOpt-in.js
+++ src/modules/volunteer/events/buttonVolunteerOpt-in.js
@@ -2,6 +2,30 @@ const { Events, channelMention } = require('discord.js');
 
 
 /**
+ * Creates and deletes ViewChannel permission for given channels.
+ * @param interaction Button interaction opting in or out.
+ * @param channel Channel to modify permissions for.
+ * @returns {Promise<void>}
+ */
+async function modifyChannelPermissions(interaction, channel) {
+	if (!channel.permissionOverwrites.cache.has(interaction.user.id)) {
+		await channel.permissionOverwrites.create(interaction.user, { ViewChannel: true });
+		await interaction.followUp({
+			content: `You have been successfully added to ${channelMention(channel.id)}!`,
+			ephemeral: true,
+		});
+	}
+	else {
+		channel.permissionOverwrites.delete(interaction.user);
+		await interaction.followUp({
+			content: 'Successfully opted out!',
+			ephemeral: true,
+		});
+	}
+
+}
+
+/**
  * Opts members in and out of the volunteer calling channel.
  * @type {{name: Events.InteractionCreate, execute(*): Promise<void>}}
  */
@@ -11,24 +35,17 @@ module.exports = {
 		if (!interaction.isButton()) return;
 		if (interaction.customId !== 'button-volunteer-opt') return;
 
-		const volunteerChannel = interaction.guild.channels.cache.get('1188685015626821734');
-		const channelPermissions = volunteerChannel.permissionOverwrites;
+		const firstReply = await interaction.reply({
+			content: 'Opting...',
+			ephemeral: true,
+		});
 
-		if (!channelPermissions.cache.has(interaction.user.id)) {
-			await channelPermissions.create(interaction.user, {
-				ViewChannel: true,
-			});
-			await interaction.reply({
-				content: `You have been added to ${channelMention('1188685015626821734')}!`,
-				ephemeral: true,
-			});
-		}
-		else {
-			channelPermissions.delete(interaction.user);
-			await interaction.reply({
-				content: 'Successfully removed!',
-				ephemeral: true,
-			});
-		}
+		const volunteerCallChannel = interaction.guild.channels.cache.get('1188685015626821734');
+		await modifyChannelPermissions(interaction, volunteerCallChannel);
+
+		const volunteerStageChannel = interaction.guild.channels.cache.get('1188987291981582408');
+		await modifyChannelPermissions(interaction, volunteerStageChannel);
+
+		await firstReply.delete();
 	},
 };
