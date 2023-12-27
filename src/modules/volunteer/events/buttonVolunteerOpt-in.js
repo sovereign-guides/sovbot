cc46d4f8e85d13ffed440f0b37c02b3bd2e870a7
zay <zaykenyon@protonmail.com>
2023-12-26 22:18:33 -0500

    fix: switch to a role based volunteering approach
    Can only have 1000 channel overwrites.

--- src/modules/volunteer/events/buttonVolunteerOpt-in.js
+++ src/modules/volunteer/events/buttonVolunteerOpt-in.js
@@ -2,31 +2,7 @@ const { Events, channelMention } = require('discord.js');
 
 
 /**
- * Creates and deletes ViewChannel permission for given channels.
- * @param interaction Button interaction opting in or out.
- * @param channel Channel to modify permissions for.
- * @returns {Promise<void>}
- */
-async function modifyChannelPermissions(interaction, channel) {
-	if (!channel.permissionOverwrites.cache.has(interaction.user.id)) {
-		await channel.permissionOverwrites.create(interaction.user, { ViewChannel: true });
-		await interaction.followUp({
-			content: `You have been successfully added to ${channelMention(channel.id)}!`,
-			ephemeral: true,
-		});
-	}
-	else {
-		channel.permissionOverwrites.delete(interaction.user);
-		await interaction.followUp({
-			content: `Successfully opted out of #${channel.name}!`,
-			ephemeral: true,
-		});
-	}
-
-}
-
-/**
- * Opts members in and out of the volunteer calling channel.
+ * Opts members in and out of the volunteer system.
  * @type {{name: Events.InteractionCreate, execute(*): Promise<void>}}
  */
 module.exports = {
@@ -35,17 +11,21 @@ module.exports = {
 		if (!interaction.isButton()) return;
 		if (interaction.customId !== 'button-volunteer-opt') return;
 
-		const firstReply = await interaction.reply({
-			content: 'Opting...',
-			ephemeral: true,
-		});
+		const volunteerRole = interaction.guild.roles.cache.get('1189396150592225290');
 
-		const volunteerCallChannel = interaction.guild.channels.cache.get('1188685015626821734');
-		await modifyChannelPermissions(interaction, volunteerCallChannel);
-
-		const volunteerStageChannel = interaction.guild.channels.cache.get('1188987291981582408');
-		await modifyChannelPermissions(interaction, volunteerStageChannel);
-
-		await firstReply.delete();
+		if (interaction.member.roles.cache.has(volunteerRole.id)) {
+			await interaction.member.roles.remove(volunteerRole);
+			await interaction.reply({
+				content: 'Successfully opted out.',
+				ephemeral: true,
+			});
+		}
+		else {
+			await interaction.member.roles.add(volunteerRole);
+			await interaction.reply({
+				content: `Successfully opted in! Airen will post recording times in ${channelMention('1188685015626821734')} and you will meet in ${channelMention('1188987291981582408')}. Press again to opt out.`,
+				ephemeral: true,
+			});
+		}
 	},
 };
